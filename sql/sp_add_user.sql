f698092e991a45b13dea59a72af564f1244d0658
Olatunde Adekola <tunde@aeternum.co>
2025-08-10 09:55:31 +0100

    Modified Stored procedure to use 3 parameters

--- sql/sp_add_user.sql
+++ sql/sp_add_user.sql
@@ -1,9 +1,8 @@
 -- Stored procedure to create User
 
 CREATE PROCEDURE sp_add_advworks_user(
-    @loginName sysname,
+    @userName sysname,
     @password  nvarchar(128),
-    @userName  sysname,
     @roleName  sysname
 )
 AS
@@ -13,14 +12,14 @@ BEGIN
     -- Create Login in master
     DECLARE @sql nvarchar(max);
 
-    SET @sql = N'CREATE LOGIN ' + QUOTENAME(@loginName) +
+    SET @sql = N'CREATE LOGIN ' + QUOTENAME(@userName) +
                N' WITH PASSWORD = ' + QUOTENAME(@password,'''');
     EXEC master.sys.sp_executesql @sql;
 
     -- Create User in AdventureWorks2022
     SET @sql = N'USE AdventureWorks2022;
                  CREATE USER ' + QUOTENAME(@userName) +
-                 N' FOR LOGIN ' + QUOTENAME(@loginName) + ';';
+                 N' FOR LOGIN ' + QUOTENAME(@userName) + ';';
     EXEC master.sys.sp_executesql @sql;
 
     -- Create Role if not exists
